bb01b0e4f0e1f4aed9cd5cf9bf7f2119644d0ada
Gwendal Plumier <gplumier@olin.edu>
2018-04-14 02:38:11 -0400

    url change

--- caffebuddy/form.py
+++ caffebuddy/form.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, request
 import json
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/open_table')
 def student():
    return render_template('table_free.html')
 
